3d6ff05fc7dead8ac711936d7bce385922505573
take <97829137+takexaz@users.noreply.github.com>
2023-07-17 23:34:52 +0900

    first commit

---
+++ MyState/dllmain.cpp
@@ -0,0 +1,56 @@
+﻿// dllmain.cpp : DLL アプリケーションのエントリ ポイントを定義します。
+#include "pch.h"
+#include "StateControllerEx.h"
+
+void stcReg(TPFILE*, STATE_INFO*, PLAYER_CACHE*);
+void stcProc(PLAYER*, STATE_INFO*);
+void stcFree(STATE_INFO*);
+
+struct MYSTATE {
+    EVAL_EXP value;
+};
+
+void stcReg(TPFILE* tpf, STATE_INFO* sinfo, PLAYER_CACHE* pcache) {
+    MYSTATE* mystate = new MYSTATE;
+    sinfo->params = mystate;
+    DWORD TEMP;
+
+    constExp(&mystate->value, 100);
+    const char* value = TPGetValue(tpf, "value");
+    if (value) {
+        SCtrlReadExpList(value, "i", pcache, &TEMP, &mystate->value);
+    }
+    return;
+}
+
+void stcProc(PLAYER* p, STATE_INFO* sinfo) {
+    MYSTATE* mystate = (MYSTATE*)sinfo->params;
+    int life = EvalExpression(p,&mystate->value, 0);
+    p->life = life;
+    return;
+}
+
+void stcFree(STATE_INFO* sinfo) {
+    MYSTATE* mystate = (MYSTATE*)sinfo->params;
+    FreeExpression(&mystate->value);
+    return;
+}
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH: {
+        addState("mystate", (DWORD)stcReg, (DWORD)stcProc, (DWORD)stcFree);
+    }
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
